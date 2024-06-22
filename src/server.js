4b99166f97b2b39141a35bb53e3f349846a53813
Vasyl Mandik <Vasik_ua@ukr.net>
2024-06-21 22:03:46 -0500

    add status code

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
         const contacts = await getAllContacts();
     res.status(200).json({
-          status: 'sucses',
+          status: 200,
             data: contacts,
         });
   });
@@ -40,7 +40,8 @@ export const setupServer = () => {
             return;
       }
      
-      res.status(200).json({
+    res.status(200).json({
+        status: 200,
         message:`Successfully found contact with id ${contactId}!`,
         data: contact,
          
@@ -54,12 +55,12 @@ export const setupServer = () => {
     next();
   });
 
-  // app.use((err, req, res, next) => {
-  //   res.status(500).json({
-  //     message: 'Something went wrong',
-  //     error: err.message,
-  //   });
-  // });
+  app.use((err, req, res) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
   
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
